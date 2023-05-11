8f657ab8d872dffb65812985dd85546754f667f5
Juan Gaviria <jgaviria@supervisa.co>
2023-05-11 14:20:25 -0500

    :cosntruction: Se deja implementado todos los componentes necesario para gestionar el Diario

--- 07-journal/src/modules/daybook/views/EntryView.vue
+++ 07-journal/src/modules/daybook/views/EntryView.vue
@@ -1,28 +1,33 @@
 <template>
-    <div class="entry-title d-flex justify-content-between p-2">
-        <div>
-            <span class="text-success fs-3 fw-bold">{{ day }}</span>
-            <span class="mx-1 fs-3">{{ month }}</span>
-            <span class="mx-2 fs-4 fw-light">{{ yearDate }}</span>
-        </div>
-        <div>
-            <button class="btn btn-danger mx-2">
-                Borrar
-                <i class="fa fa-trash-alt"></i>
-            </button>
 
-            <button class="btn btn-primary">
-                Subir foto
-                <i class="fa fa-upload"></i>
-            </button>
-        </div>
-    </div>
+    <template v-if="entry">
+        <div
+        class="entry-title d-flex justify-content-between p-2">
+            <div>
+                <span class="text-success fs-3 fw-bold">{{ day }}</span>
+                <span class="mx-1 fs-3">{{ month }}</span>
+                <span class="mx-2 fs-4 fw-light">{{ yearDate }}</span>
+            </div>
+            <div>
+                <button class="btn btn-danger mx-2">
+                    Borrar
+                    <i class="fa fa-trash-alt"></i>
+                </button>
 
-    <hr>
-    <div class="d-flex flex-column px-3 h-75">
-        <textarea placeholder="¿Que sucesió hoy?"
-        v-model="entry.text"></textarea>
-    </div>
+                <button class="btn btn-primary">
+                    Subir foto
+                    <i class="fa fa-upload"></i>
+                </button>
+            </div>
+        </div>
+        <hr>
+        <div class="d-flex flex-column px-3 h-75">
+            <textarea placeholder="¿Que sucesió hoy?"
+            v-model="entry.text"></textarea>
+        </div>
+    </template>
+
+
 
     <Fab icon="fa-save"/>
 
@@ -49,7 +54,7 @@ export default {
     methods: {
         loadEntry () {
             const entry = this.getEntryById(this.id)
-            if (!entry) this.$router.push({ name: 'no-entry' })
+            if (!entry) return this.$router.push({ name: 'no-entry' })
             this.entry = entry
         }
     },
@@ -75,6 +80,11 @@ export default {
     },
     created () {
         this.loadEntry()
+    },
+    watch: {
+        id ( ) {
+            this.loadEntry()
+        }
     }
 }
 </script>
